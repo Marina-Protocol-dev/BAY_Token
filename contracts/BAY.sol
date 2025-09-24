6e9887ab7b3c6b53ba57eb0bd1c99fc9207dc3d1
dawn-island <toqurtja@gmail.com>
2025-09-24 16:00:20 +0900

    remove ERC20Permit

--- contracts/BAY.sol
+++ contracts/BAY.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /**
  * @title BAY
  * @notice Marina Protocol utility token
  * @dev Standard ERC20 token with permit functionality for gasless approvals
  */
-contract BAY is ERC20, ERC20Permit {
+contract BAY is ERC20 {
     /// @notice Total supply of BAY tokens (1 billion)
     uint256 public constant TOTAL_SUPPLY = 1_000_000_000 * 10**18;
 
@@ -19,7 +18,6 @@ contract BAY is ERC20, ERC20Permit {
      */
     constructor(address recipient)
         ERC20("BAY", "BAY")
-        ERC20Permit("BAY")
     {
         _mint(recipient, TOTAL_SUPPLY);
     }
